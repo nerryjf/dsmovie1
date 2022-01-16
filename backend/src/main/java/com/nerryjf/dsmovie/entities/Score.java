9439bb7221e4cd2d46b72f8cb908f4755315a32d
Nerry Jean Francois <nerryjf80@gmail.com>
2022-01-16 00:53:21 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/nerryjf/dsmovie/entities/Score.java
@@ -0,0 +1,35 @@
+package com.nerryjf.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	private Double value;
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+	public ScorePK getId() {
+		return id;
+	}
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+	public Double getValue() {
+		return value;
+	}
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+
+}
